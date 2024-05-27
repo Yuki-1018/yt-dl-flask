011b22397bfd1e315e4af6c961b932b242fb845d
Yuki <86597897+Yuki-1018@users.noreply.github.com>
2024-05-27 14:16:43 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,31 @@
+from flask import Flask, render_template, request, jsonify
 import yt_dlp
-from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == "POST":
-        video_url = request.form["video_url"]
+    if request.method == 'POST':
+        video_url = request.form['video_url']
 
         ydl_opts = {
-            "format": "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best",
-            "outtmpl": "%(id)s.%(ext)s",
+            'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
+            'noplaylist': True,
         }
 
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info = ydl.extract_info(video_url, download=False)
-            video_id = info["id"]
+            video_format = ydl.get_suitable_formats(info)[0]
+            video_link = video_format['url']
+            audio_link = None
+            for fmt in ydl.get_suitable_formats(info):
+                if fmt['ext'] == 'm4a':
+                    audio_link = fmt['url']
+                    break
 
-            mp4_url = url_for("download", filename=f"{video_id}.mp4")
-            mp3_url = url_for("download", filename=f"{video_id}.m4a")
+        return jsonify({'video_link': video_link, 'audio_link': audio_link})
 
-            return render_template("index.html", mp4_url=mp4_url, mp3_url=mp3_url)
+    return render_template('index.html')
 
-    return render_template("index.html")
-
-@app.route("/download/<filename>")
-def download(filename):
-    return redirect(url_for("static", filename=filename))
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
